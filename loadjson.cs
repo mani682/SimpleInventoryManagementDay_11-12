b0afed52e404241021c3eff7c38e1a187a24693d
mani682 <manichandanamiriyala682@gmail.com>
2022-10-18 09:09:58 +0530

    simple inventory management

---
+++ loadjson.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockAccountManagement
+{
+     public class loadjson
+    {
+        public Model Read(String path)
+        {
+            using (StreamReader file = new StreamReader(path))
+            {
+                try
+                {
+                    string json = file.ReadToEnd();
+                    return jsonConvert.DeserializeObject<Model>(json);
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    return null;
+                }
+
+            }
+        }
+    }
+}
