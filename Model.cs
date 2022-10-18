badbd7355891b33f8724e0c714c98c6e51465ad4
mani682 <manichandanamiriyala682@gmail.com>
2022-10-18 09:11:05 +0530

    simple inventory management

---
+++ Model.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockAccountManagement
+{
+    public class Model
+    {
+        public List<StockMarket> stockMarket;
+
+    }
+    public class StockMarket
+    {
+        public string shareName { get; set; }
+        public int numberOfShare { get; set; }
+        public int sharePrice { get; set; }
+    }
+}
