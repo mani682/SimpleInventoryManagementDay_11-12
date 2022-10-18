d652c6839669aa17c35696e177aca09eb5db51d5
mani682 <manichandanamiriyala682@gmail.com>
2022-10-18 09:07:53 +0530

    simple inventory management

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Reflection;
+
+namespace JsonDemo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            int totalStockValue = 0;
+            string filePath = @"C:\Users\ishwa\source\repos\c#208\JasonDemo\JasonDemo\SimpleJsonFile.json";
+            Loadjson loadJson = new Loadjson();
+            Model data = new Model();
+            data = loadJson.Read(filePath);
+
+             Console.WriteLine(data.stockMarket[0].numberOfShare * data.stockMarket[0].sharePrice);
+
+            Console.WriteLine("---------------------------------");
+            for (int i = 0; i < data.stockMarket.Count; i++)
+            {
+                Console.WriteLine("Share Name: " + data.stockMarket[i].shareName);
+                Console.WriteLine("Number of Sahare: " + data.stockMarket[i].numberOfShare);
+                Console.WriteLine("Share Price: " + data.stockMarket[i].sharePrice);
+                int stockValue = data.stockMarket[i].numberOfShare * data.stockMarket[i].sharePrice;
+                Console.WriteLine("Total stock value of {0} is : {1}", data.stockMarket[i].shareName, stockValue);
+                totalStockValue += stockValue;
+                Console.WriteLine("---------------------------------");
+            }
+            Console.WriteLine("value of total stocks  is: {0}", totalStockValue);
+        }
+    }
+}
